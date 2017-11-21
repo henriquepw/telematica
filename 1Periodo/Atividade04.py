c1e25117e3bab0bc19af9c9b05d9b13e1b28d51a
Henrique Martins <enrque.ns@gmail.com>
2017-11-21 18:35:46 -0300

    Atividade de estrutura condicional

---
+++ 1Periodo/Atividade04.py
@@ -0,0 +1,21 @@
+# Feito em: 21/11/2017
+# Data da entrega 23/11/2017
+
+def questao01():
+    valores = []
+    maior = medio = 0.0
+    for x in range(0, 3):
+        valores.append(float(input("Digite o valor: ")))
+        if maior < valores[x]:
+            maior = valores[x]
+    for x in range(0, 3):
+        if medio < valores[x] < maior:
+            medio = valores[x]
+
+    print("Soma dos 2 maiores = ", maior + medio)
+
+
+def questao02():
+    dados = input()
+    numero = [float(x) for x in dados.split(" ")]
+    print("%.2f" % (numero[0]/numero[1]))
