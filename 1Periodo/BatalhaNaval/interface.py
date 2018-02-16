edaaae50c418cf5865b8d04958487fb4f746b1e6
Henrique Martins <enrque.ns@gmail.com>
2018-02-16 08:03:38 -0300

    projeto

---
+++ 1Periodo/BatalhaNaval/interface.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+def regras():
+    print()
+
+
+def menu():
+    print('** Batalha Naval v1.0 **')
+    print()
+    print('Iniciar')
+    print('Creditos')
+
+if __name__ == '__main__':
+    menu()
