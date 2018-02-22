5ae5da3c177885ea1f2a1120f76000db5f4d4774
Henrique Martins <enrque.ns@gmail.com>
2018-02-22 15:36:27 -0300

    jogador

--- 1Periodo/BatalhaNaval/main.py
+++ 1Periodo/BatalhaNaval/main.py
@@ -1,9 +1,26 @@
-# encoding: utf-8
+import copy
 from msvcrt import getch
 from os import system
+from .opponent import *
 
+# variaveis
+
+# padrão ANSI
 clear = '\033[m'
 destaque = '\033[1;33m'
+
+tamanho = 10
+letra = []
+sequencia = []
+
+a = ord('A')
+for l in range(tamanho):
+    letra.append(chr(a + l ))
+    sequencia.append(l)
+
+mapa = []
+mapa_jogador = []
+mapa_maquina = []
 
 
 def regras():
@@ -18,7 +35,7 @@ def regras():
     print(destaque, ' Tabuleiro: ', clear)
     print('    - São duas grelhas para cada jogador.')
     print('    - Uma que representa a disposição dos barcos do jogador, e outra que representa a do oponente. ')
-    print('    - As grelhas são quadradas(10 x 10), estando identificadas na horizontal por números e na vertical'
+    print('    - As grelhas são quadradas(12 x 12), estando identificadas na horizontal por números e na vertical'
           ' por letras. \n')
 
     print(destaque, ' Preparação: ', clear)
@@ -37,13 +54,9 @@ def regras():
     getch()
 
 
-def play():
-    print()
-
-
 def creditos():
     print(destaque, 'Creditos: ', clear)
-    print('  - Henrique M. Miranda')
+    print('  - Antonio D. S. C. Junior')
     print('  - Henrique M. Miranda')
     print('  - Henrique M. Miranda')
     print(destaque, '\nPressione qualquer tecla para voltar ao menu ...', clear)
@@ -71,8 +84,9 @@ def menu(se):
     print(destaque, '*Pressione ESC para fechar ', clear)
 
 
+# interação
 def intera(init=0, final=2, se=0):
-    options = {0: play,
+    options = {0: main,
                1: regras,
                2: creditos, }
 
@@ -98,6 +112,220 @@ def intera(init=0, final=2, se=0):
             options[se]()
 
 
+# feito por junior
+
+def imprime_mapa(var, mapa):
+    jogador = "Computador"
+    if var == "j":
+        jogador = "Player"
+
+    print("\n O " + jogador + " tem essa visao: \n")
+
+    # cria o mapa e exibe
+    for i in range(len(letra)):
+        print(letra[i], end="  ")
+        for j in range(10):
+            print("O", end=" ")
+        print()
+    print("   ", end="")
+
+    for j in sequencia:
+        print(str(j), end=" ")
+
+    return mapa
+
+
+def print_mapas():
+    numeros = '[]'
+    for x in range(tamanho):
+        numeros += ' ' + x
+
+    print(numeros)
+    for x in range(tamanho):
+        
+
+
+
+def posiciona_navio(mapa, navio, var, sentido, x, y):
+    if sentido == "h":
+        for i in range(navio):
+            mapa[x][y + i] = var
+    elif sentido == "v":
+        for i in range(navio):
+            mapa[x + i][y] = var
+    return mapa
+
+
+def player_posiciona_navio(mapa, embarcacoes):
+    for navio in embarcacoes.keys():
+        validador = False
+        while not validador:
+            imprime_mapa("j", mapa)
+            print("\nPosicionando o : " + navio)
+            x, y = recebe_coordenada()
+            sentido = h_ou_v()
+            validador = verifica(mapa, embarcacoes[navio], x, y, sentido)
+
+            if not validador:
+                print("Nao pode colocar o navio neste local.\nTente coloca-lo em outro lugar do mapa.")
+                input("Aperte enter para prosseguir")
+
+        mapa = posiciona_navio(mapa, embarcacoes[navio], navio[0], sentido, x, y)
+        imprime_mapa("j", mapa)
+
+    input("Navio posicionado com sucesso. Aperte enter para continuar")
+    return mapa
+
+
+def verifica(mapa, navio, x, y, sentido):
+    if sentido == "h" and y + navio > 10:
+        return False
+    elif sentido == "v" and x + navio > 10:
+        return False
+    else:
+        if sentido == "h":
+            for i in range(navio):
+                if mapa[x][y + i] != -1:
+                    return False
+        elif sentido == "v":
+            for i in range(navio):
+                if mapa[x + i][y] != -1:
+                    return False
+    return True
+
+
+def h_ou_v():
+    while True:
+        var = input("Escolha horizontal (h) ou vertical (v): ").lower()
+        if var == "h" or var == "v":
+            return var
+        else:
+            print("Escolha invalida, Tente h para horizontal ou v para vertical")
+
+
+def recebe_coordenada():
+    while True:
+        y = int(input("Escolha a coluna no intervalo de 0-9: "))
+        x = input("Escolha a linha no intervalo A-J: ").upper()
+
+        if all(z != y for z in sequencia):
+            print("Coordenada Y invalida tente novamente.")
+        elif all(z != x for z in letra):
+            print("Coordenada X invalida tente novamente.")
+        else:
+            for num in range(len(letra)):
+                if x == letra[num]:
+                    x2 = num
+                    break
+            return x2, y
+
+
+def vencedor(mapa):
+    for i in range(10):
+        for j in range(10):
+            if mapa[i][j] != -1 and mapa[i][j] != "*" and mapa[i][j] != "#":
+                return False
+    return True
+
+
+def que_tiro_foi_esse(mapa, x, y):
+    if mapa[x][y] == "A":
+        navio = "Porta Avioes"
+    elif mapa[x][y] == "B":
+        navio = "Encouraçado"
+    elif mapa[x][y] == "S":
+        navio = "Submarino"
+    elif mapa[x][y] == "D":
+        navio = "Cruzados"
+
+    mapa[-1][navio] -= 1
+    if mapa[-1][navio] == 0:
+        print(navio + " atingido")
+
+
+def movimento(mapa, x, y):
+    if mapa[x][y] == -1:
+        return "Errou!"
+    elif mapa[x][y] == '*' or mapa[x][y] == "#":
+        return "Tente Novamente"
+    else:
+        return "Acertou!"
+
+
+def player_atirando(mapa):
+    while True:
+        x, y = recebe_coordenada()
+        mov = movimento(mapa, x, y)
+        if mov == "Acertou!":
+            print("Atingiu " + str(x) + "," + str(y))
+            que_tiro_foi_esse(mapa, x, y)
+            mapa[x][y] = "#"
+            if vencedor(mapa):
+                return "VITORIA"
+        elif mov == "Errou!":
+            print("O tiro " + str(x) + "," + str(y) + " nao atingiu nenhum alvo.")
+
+        if mov != "Tente Novamente":
+            return mapa
+
+
+def main():
+    # tipos de navios
+    navios = {"Porta Avioes": 5,
+              "Encouraçado": 4,
+              "Submarino": 2,
+              "Cruzados": 3}
+
+    # setando um mapa 10x10 branco
+    mapa = []
+    for i in range(10):
+        linha = []
+        for j in range(10):
+            linha.append(-1)
+        mapa.append(linha)
+
+    # setando player/maquina
+    mapa_jogador = copy.deepcopy(mapa)
+
+    # adicionando navios
+    mapa_jogador.append(copy.deepcopy(navios))
+
+    # posicionando navios
+    mapa_jogador = player_posiciona_navio(mapa_jogador, navios)
+
+    # loop principal do jogo
+    while (1):
+
+        # movimento do jogador
+        imprime_mapa("c", mapa_maquina)
+        mapa_maquina = player_atirando(mapa_maquina)
+
+        # verifica se player ganhou
+        if mapa_maquina == "VITORIA":
+            print("Jogador vendeu!!")
+            quit()
+
+        # exibe o mapa do maquina atual
+        imprime_mapa("c", mapa_maquina)
+        input("Pressione enter para finalizar a jogada")
+
+        '''
+        computer move
+        felipe tem que colocar aqui a jogada da maquina
+        mapa_jogador = maquina_atirando(mapa_jogador)
+        '''
+
+        # verifica a jogada da maquina
+        if mapa_jogador == "VITORIA":
+            print("A Maquina VENCEU!!!")
+            quit()
+
+        # exibe o mapa do jogador atual
+        imprime_mapa("j", mapa_jogador)
+        input("Para finalizar o turno da maquina pressione ENTER")
+
+
 if __name__ == '__main__':
     system('cls')
-    intera()
+    main()
+    # intera()
--- 1Periodo/BatalhaNaval/opponent.py
+++ 1Periodo/BatalhaNaval/opponent.py
@@ -1,8 +1,9 @@
 # encoding: utf-8
 from random import randint
 
-attack = {}
-defense = {}
+attack = []
+defense = []
+tamanho = 12
 
 '''
 
@@ -14,56 +15,109 @@ defense = {}
 '''
 
 
+# range de letra a letra
 def crange(c1, c2):
     yield from range(ord(c1), ord(c2) + 1)
 
 
+# inicia os tabulheiros
 def init():
-    for c in crange('A', 'J'):
-        attack[chr(c)] = [0 for k in range(10)]
-        defense[chr(c)] = [0 for k in range(10)]
+    for c in range(tamanho):
+        attack.append([0 for k in range(tamanho)])
+        defense.append([0 for k in range(tamanho)])
 
     for i in range(2, 6):
         preencher(i, 6 - i)
 
 
+# verifica os arredores das posições dos navios
+def verificar(linha, coluna, confirmed):
+    global defense
+
+    if linha == tamanho - 1:
+        linha_verificar = [linha - 1, linha + 1]
+    elif linha == 0:
+        linha_verificar = [linha, linha + 2]
+    else:  # linha > 0:
+        linha_verificar = [linha - 1, linha + 2]
+
+    if coluna == tamanho - 1:
+        coluna_verificar = [coluna - 1, coluna + 1]
+    elif coluna == 0:
+        coluna_verificar = [coluna, coluna + 2]
+    else:  # coluna > 0:
+        coluna_verificar = [coluna - 1, coluna + 2]
+
+    for li in range(linha_verificar[0], linha_verificar[1]):
+        for co in range(coluna_verificar[0], coluna_verificar[1]):
+            print('li ', li)
+            print('co', co)
+            if defense[li][co] != 0:
+                confirmed = False
+                print('entrou')
+    return confirmed
+
+
+# preencher de forma aleatoria segundo as regras do jogo o tabulheiro de defesa da maquina
 def preencher(tipo, quantidade):
+    global defense
     c = 0
+
     while c < quantidade:
+        # 0 = horizontal ; 1 = Vertical
         sentido = randint(0, 1)
 
         if sentido == 0:
-            linha = chr(randint(ord('A'), ord('J')))
-            coluna = randint(0, 10 - tipo)
+            # aleatorizando uma posição
+            linha = randint(0, tamanho - 1)
+            coluna = randint(0, tamanho - tipo)
 
+            # confirmando se a posição que foi aleatorizada está livre
             confirmed = True
             for i in range(tipo):
+                print('linha', linha)
+                print('coluna', coluna)
+                print('coluna + tipo', coluna + i)
+                confirmed = verificar(linha, coluna + i, confirmed)
+
                 if defense[linha][coluna + i] != 0:
                     confirmed = False
 
+            # se for confirmado, preenche
             if confirmed:
                 for i in range(tipo):
                     defense[linha][coluna + i] = tipo
                 c += 1
 
+                bloco(defense)
+                print(confirmed)
+                print('------------')
+
         else:
-            linha = chr(randint(ord('A'), ord('K') - tipo))
-            coluna = randint(0, 9)
+            linha = randint(0, tamanho - tipo)
+            coluna = randint(0, tamanho - 1)
 
             confirmed = True
             for i in range(tipo):
-                if defense[chr(ord(linha) + i)][coluna] != 0:
-                    confirmed = False
+                print('linha', linha)
+                print('coluna', coluna)
+                print('linha + tipo', linha + i)
+                confirmed = verificar(linha + i, coluna, confirmed)
 
             if confirmed:
                 for i in range(tipo):
-                    defense[chr(ord(linha) + i)][coluna] = tipo
+                    defense[linha + i][coluna] = tipo
                 c += 1
 
+                bloco(defense)
+                print(confirmed)
+                print('------------')
 
+
+# print o tabulheiro
 def bloco(bloc):
-    for c in crange('A', 'J'):
-        print(bloc[chr(c)])
+    for c in range(tamanho):
+        print(c, ' ', bloc[c])
 
 
 if __name__ == '__main__':
