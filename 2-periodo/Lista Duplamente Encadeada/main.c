1316f4b23964cdf25a7ce750a58064b7cacb974c
Henrique Martins <enrque.ns@gmail.com>
2018-05-20 13:25:33 -0300

    pop

--- 2-periodo/Lista Duplamente Encadeada/main.c
+++ 2-periodo/Lista Duplamente Encadeada/main.c
@@ -14,10 +14,17 @@ typedef struct list{
 List *createList();
 node *alloc(int);
 int isEmpty(List *);
+void options();
 
 void push(List *, int);
 void pushFirst(List *, int);
 void pushIn(List *, int);
+
+void popIn(List *, int);
+void popFirst(List *);
+void pop(List *);
+void popIndex(List *, int);
+void popAll(List *);
 
 void menu(List *);
 void clean();
@@ -25,6 +32,10 @@ void clean();
 
 int main(void){
 	List *alunos = createList();
+	
+	for(int i=0; i < 10; i++)
+		pushIn(alunos, i);
+
 	menu(alunos);
 	
 	return 0;
@@ -124,6 +135,109 @@ void pushIn(List *list, int num){
   
 }
 
+void pop(List *list){
+	if(isEmpty(list)){
+		printf("Nao existe item para ser removido");
+	} else if (list->init == list->last){
+		free(list->init);
+		list->last = NULL;
+		list->init = NULL;
+		list->size--;
+	}  else {
+		node *aux = list->last;
+		list->last = list->last->ant;
+		list->last->prox = NULL;
+		list->size--;
+		free(aux);
+	}
+}
+
+void popFirst(List *list){
+	if(isEmpty(list)){
+		printf("Nao existe item para ser removido");
+	} else if (list->init == list->last){
+		free(list->init);
+		list->last = NULL;
+		list->init = NULL;
+		list->size--;
+	} else {
+		node *aux = list->init;
+		list->init = list->init->prox;
+		list->init->ant = NULL;
+		list->size--;
+		free(aux);	
+	}
+}
+
+void popIn(List *list, int num){
+	if(isEmpty(list)){
+		printf("Nao existe item para ser removido. \n");
+	} else if(num == list->last->num){
+		pop(list);
+	} else if (num == list->init->num){
+		popFirst(list);
+	} else {
+		node *auxI = list->init->prox;
+		node *auxF = list->last->ant;
+		
+		while ( (num != auxI->num) && (num != auxF->num) 
+		&& (auxI->prox != NULL)){
+			auxI = auxI->prox;
+			auxF = auxF->ant;
+		}
+		
+		node *aux;
+		if (num == auxF->num) {
+			aux = auxF;
+		} else if (num == auxI->num) {
+			aux = auxI;
+		} else {
+			printf("Item nao encontrado... \n");
+			return;	
+		}
+		
+		aux->ant->prox =  aux->prox;
+		aux->prox->ant =  aux->ant;
+		list->size--;
+		free(aux);
+	}
+		
+}
+
+void popIndex(List *list, int pos){
+	node *aux = list->init;
+	int i;
+	while ((i < pos) && (aux != NULL)){
+		aux = aux->prox;
+		i++;
+	}
+	
+	if(aux == NULL){
+		printf("posicao invalida \n");
+	} else if(aux == list->last){
+		pop(list);
+	} else if (aux == list->init){
+		popFirst(list);
+	} else {
+		aux->ant->prox = aux->prox;
+		aux->prox->ant = aux->ant;
+		list->size--;
+		free(aux);
+	}
+}
+
+void popAll(List *list){
+	node *aux;
+	while (list->init != NULL){
+		aux = list->init;
+		list->init = list->init->prox;
+		free(aux);
+	}
+	
+	list->last = NULL;
+	list->size = 0;
+}
+
 void showFirst(List *list){
 	if(!isEmpty(list)) printf("Primeiro numero: %d \n", list->init->num);
 	else printf("Lista vazia. \n");
@@ -145,7 +259,7 @@ void showAll(List *list, int begin){
 	} else {
 		node *aux = list->last;
 		while(aux != NULL){
-			printf("umero: %d \n", aux->num);
+			printf("Numero: %d \n", aux->num);
 			aux = aux->ant;
 		}
 	}
@@ -155,24 +269,7 @@ void showAll(List *list, int begin){
 void menu(List *list){
 	int se = -1;
 	while (se != 0){
-		printf("01. Inserir \n");
-		printf("02. verificar se esta vazia \n");
-		
-		printf("03. Exibir lista \n");
-		printf("04. Exibir o primeiro \n");
-		printf("05. Exibir o ultimo \n");
-		printf("06. Pesquisar aluno por nome \n");
-		
-		printf("07. Tamanho \n");
-		
-		printf("08. Remover o primeiro \n");
-		printf("09. Remover o ultimo \n");
-		printf("10. Remover aluno por nome \n");
-
-		printf("11. Inserir no inicio \n");
-		printf("12. Inserir ordenado \n");
-		
-		printf("00. Sair \n");
+		options();
 		scanf("%d", &se);
 		
 		int num;
@@ -184,11 +281,8 @@ void menu(List *list){
 				clean();
 				break;
 			case 2:
-				if(isEmpty(list))
-					printf("Lista esta vazia! \n");
-				else 
-					printf("Lista nao esta vazia! \n");
-				
+				if(isEmpty(list)) printf("Lista esta vazia! \n");
+				else printf("Lista nao esta vazia! \n");
 				clean();
 				break;
 			case 3:
@@ -196,10 +290,11 @@ void menu(List *list){
 				clean();
 				break;
 			case 4:
-				showFirst(list);
+				showAll(list, 0);
 				clean();
 				break;
 			case 5:
+				showFirst(list);
 				showLast(list);
 				clean();
 				break;
@@ -213,34 +308,40 @@ void menu(List *list){
 				printf("Tamanho: %d \n", list->size);
 				clean();
 				break;
-			/*case 8:
+			case 8:
 				popFirst(list);
 				clean();
 				break;
 			case 9:
-				popLast(list);
+				pop(list);
 				clean();
 				break;
 			case 10:
-				printf("Digite o nome do aluno: ");
-				scanf(" %[^\n]s", name);
-				popByName(list, name);
+				printf("Digite o numero: ");
+				scanf("%d", &num);
+				popIn(list, num);
 				clean();
-				break;*/
+				break;
 			case 11:
+				printf("Digite o numero: ");
+				scanf("%d", &num);
+				popIndex(list, num);
+				clean();
+				break;
+			case 12:
+				popAll(list);
+				clean();
+				break;
+			case 13:
 				printf("Digite o numero: ");
 				scanf("%d", &num);
 				pushFirst(list, num);
 				clean();
 				break;
-			case 12:
+			case 14:
 				printf("Digite o numero: ");
 				scanf("%d", &num);
 				pushIn(list, num);
-				clean();
-				break;
-			case 13:
-				list = createList();
 				clean();
 				break;
 			case 0:
@@ -253,6 +354,29 @@ void menu(List *list){
 		}
 		
 	}	
+}
+
+void options(){
+	printf("01. Inserir \n");
+	printf("02. verificar se esta vazia \n");
+		
+	printf("03. Exibir lista I/F\n");
+	printf("04. Exibir lista F/I \n");
+	printf("05. Exibir o primeiro e o ultimo \n");
+	printf("06. Pesquisar aluno por nome \n");
+		
+	printf("07. Tamanho \n");
+		
+	printf("08. Remover o primeiro \n");
+	printf("09. Remover o ultimo \n");
+	printf("10. Remover numero \n");
+	printf("11. Remover indice \n");
+	printf("12. Apagar lista \n");
+
+	printf("13. Inserir no inicio \n");
+	printf("14. Inserir ordenado \n");
+		
+	printf("00. Sair \n");
 }
 
 void clean(){
