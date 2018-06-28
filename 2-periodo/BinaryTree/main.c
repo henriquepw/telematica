b0dc5c7115f883b30d930dd93aa6935124575ce5
Henrique Martins <enrque.ns@gmail.com>
2018-06-28 19:12:55 -0300

    q4

--- 2-periodo/BinaryTree/main.c
+++ 2-periodo/BinaryTree/main.c
@@ -6,126 +6,166 @@ typedef struct elem {
 	struct elem *left, *right;	
 } Node;
 
-Node *createList();
 int isEmpty(Node *root);
+Node *createTree();
+Node *getNode(Node *tree, int value);
+int getChild(Node *node);
 
 void preOrder(Node *tree);
 void postOrder(Node *tree);
 void inOrder(Node *tree);
 
 void insert(Node **root, int value);
+void remov(Node **root, int value);
+void swap(Node **root, int value1, int value2);
+
+Node *max(Node *tree);
+Node *sum(Node *tree);
 
 void menu(Node *root);
-void options();
+int options();
 void clean();
 
 int main (){
-	Node *root = createList();
+	Node *root = createTree();
 	menu(root);
 	
 	return 0;
 }
 
-Node *createList(){
+Node *createTree(){
 	Node *tree = NULL;
 	return tree;
 }
 
-int isEmpty(Node *root){
-	return root == NULL;
+int isEmpty(Node *node){
+	return node == NULL;
 }
 
 void preOrder(Node *tree){
-	if(tree != NULL){
-		printf("%d ", tree->value);
+	if(!isEmpty(tree)){
+		printf("%d \n", tree->value);
 		preOrder(tree->left);
 		preOrder(tree->right);
-		printf("\n");
 	}
 }
 
 void postOrder(Node *tree){
-	if(tree != NULL){
+	if(!isEmpty(tree)){
 		preOrder(tree->left);
 		preOrder(tree->right);
-		printf("%d ", tree->value);
-		printf("\n");
+		printf("%d \n", tree->value);
 	}
 }
 
 void inOrder(Node *tree){
-	if(tree != NULL){
+	if(!isEmpty(tree)){
 		preOrder(tree->left);
-		printf("%d ", tree->value);
+		printf("%d \n", tree->value);
 		preOrder(tree->right);
-		printf("\n");
 	}
 }
 
-void insert(Node **root, int value){
-	if (*root == NULL){
+void insert(Node **root, int value){	
+	if (isEmpty(*root)){
 		*root = (Node *) malloc(sizeof(Node));
 		(*root)->value = value;
 		(*root)->left = NULL;
-		(*root)->right = NULL;	
+		(*root)->right = NULL;
 	} else if(value < ((*root)->value))
 		insert(&((*root)->left), value);
+		
+	else if(value > ((*root)->value))
+		insert(&((*root)->right), value); 
+}
+
+Node *max(Node *tree){
+	if(!isEmpty(tree->right)) max(tree->right);
+	else return tree;
+}
+
+Node *sum(Node *tree){
+	
+}
+
+void remov(Node **root, int value){
+	if(isEmpty(*root)) printf("Arvore vazia \n");	
 	else
-		insert(&((*root)->right), value);
+		switch(getChild(*root)){
+			case 1:
+				 
+				break;
+			case 2:
+				 
+				break;
+			case 3:
+				 
+				break;
+		}
+}
+
+Node *getNode(Node *tree, int value){
+	if(isEmpty(tree)) return NULL;
+	else if(tree->value == value) return tree;
+	else if (tree->value > value) getNode(tree->left, value);
+	else if (tree->value < value) getNode(tree->right, value);
+}
+
+int getChild(Node *node){
+	if((node->left == NULL) && (node->right == NULL)) return 0;
+	else if((node->left != NULL) || (node->right != NULL)) return 1;
+	else return 2;
 }
 
 void menu(Node *root){
-	int se = -1;
-	while (se != 0){
-		options();
-		scanf("%d", &se);
-
+	while(1) {
+		clean();
 		int num;
-		switch(se){
+		switch(options()){
 			case 1:
 				printf("Digite o numero: ");
 				scanf("%d", &num);
 				insert(&root, num);
-				clean();
 				break;
-			case 2:
+			case 2: postOrder(root); break;
+			case 3:
 				if(isEmpty(root)) printf("Arvore esta vazia! \n");
 				else printf("Arvore nao esta vazia! \n");
-				clean();
 				break;
-			case 3:
-				preOrder(root);
-				clean();
-				break;
-			case 4:
-				postOrder(root);
-				clean();
-				break;
-			case 5:
-				inOrder(root);
-				clean();
-				break;
+			case 4: printf("Maior: %d \n", max(root)->value) break;
+			case 5: break;
+			case 6: break;
+			case 7: break;
+			case 8: break;
+			case 9: break;
+			case 10: break;
 			case 0: exit(1);
-			default:
-				system("cls");
-				printf("Comando invalido. Digite novamente \n");
+			default: printf("Comando invalido. ");
 		}
-
+		system("pause");
 	}
 }
 
-void options(){
-	printf("01. Inserir \n");
-	printf("02. Verificar se esta vazia \n");
-
-	printf("03. Pre ordem \n");
-	printf("04. Pos ordem \n");
-	printf("05. Em ordem \n");
+int options(){
+	printf("Questao 01. \n");
+	printf("Questao 02. \n");
+	printf("Questao 03. \n");
+	printf("Questao 04. \n");
+	printf("Questao 05. \n");
+	printf("Questao 06. \n");
+	printf("Questao 07. \n");
+	printf("Questao 08. \n");
+	printf("Questao 09. \n");
+	printf("Questao 10. \n");
+	printf("Questao 11. \n");
+	printf("Sair    00. \n");
 	
-	printf("00. Sair \n");
+	int se = -1;
+	scanf("%d", &se);
+	return se;
 }
 
 void clean(){
-	system("pause");
 	puts("\x1b[H\x1b[2J");
+	system("cls");
 }
