352c6f11e12e5bb6b2876498f51f6e4cc862e545
Henrique Martins <enrque.ns@gmail.com>
2018-09-12 16:56:08 -0300

    nc -l

--- 3-periodo/Cpp/server_nc_linux.cpp
+++ 3-periodo/Cpp/server_nc_linux.cpp
@@ -14,33 +14,42 @@ int main() {
   struct sockaddr_in client_addr;
   struct sockaddr_in addr;
 
+  int port = PORT;
+  while (true) {
+    cin >> port;
+
+    if (port >= 0 and port <= 1024)
+      cout << "Porta reservada, tente outra" << '\n';
+    else
+      break;
+  }
+
   addr.sin_family = AF_INET;
   addr.sin_addr.s_addr = htonl(INADDR_ANY);
-  addr.sin_port = htons(PORT);
+  addr.sin_port = htons(port);
 
   // Criando o socket do servidor e do cliente
   int server = socket(AF_INET, SOCK_STREAM, 0), client;
   socklen_t client_size = sizeof client_addr;
 
-  // lidando o socket ao ip
-  bind(server, (struct sockaddr *)&addr, sizeof addr);
+  // ligando o socket ao ip e limitando o numero de conexões
+  bind(server, (struct sockaddr *)&addr, sizeof addr);=
+  listen(server, 1);
 
-  // permintir que o socket receba conexões (socket, numero de conexões)
-  listen(server, 5);
-
-  cout << "/* Chat init */" << '\n';
-  while (1) {
+  cout << "/* Chat ininicado na porta " << port << " */ \n";
+  while (true) {
     // aceitando o pedido do cliente
     client = accept(server, (struct sockaddr *)&client_addr, &client_size);
 
-    while (1) {
+    while (client > 0) {
       char buff[255];
       int x = recv(client, buff, sizeof buff, 0);
-      send(client, buff, x, 0);
 
       cout << buff;
-      fflush(stdout);
+      fflush(stdin);
     }
+
+    close(client);
   }
 
   return 0;
