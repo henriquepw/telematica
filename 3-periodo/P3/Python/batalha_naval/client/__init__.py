86f7df455b7c1d76b50565697e98e72c26d247e7
Henrique Martins <enrque.ns@gmail.com>
2018-11-10 14:12:03 -0300

    P3 -> project 1 -> filling map

--- 3-periodo/P3/Python/batalha_naval/client/__init__.py
+++ 3-periodo/P3/Python/batalha_naval/client/__init__.py
@@ -9,22 +9,25 @@ map_atk = []
 TAM = 14
 
 
-def print_block(block):
+def print_block(block, visible):
     if block == 1:
         print('\033[7;31m[X]\033[7;30m \033[m', end='')
     elif block == 6:
         print('\033[7;32m[V]\033[7;30m \033[m', end='')
     else:
-        print('\033[7;30m[ ] \033[m', end='')
+        if visible:
+            print('\033[7;30m[',block,'] \033[m', end='')
+        else:
+            print('\033[7;30m[ ] \033[m', end='')
 
 
-def print_line(matrix, i):
+def print_line(matrix, i, visible):
     for j in range(1, TAM - 1):
-        print_block(matrix[i][j])
+        print_block(matrix[i][j], visible)
 
 
-def print_maps():
-    print('Mapa do Cliente', ' ' * 46, 'Mapa do Servidor', '\n   ', end='')
+def print_maps(visible=False):
+    print('Mapa de Ataque', ' ' * 46, 'Mapa de defesa', '\n   ', end='')
     for k in range(1, TAM - 1):
         print(str(k), end='   ' if k < 9 else '  ')
 
@@ -35,13 +38,13 @@ def print_maps():
     print()
     for i in range(1, TAM - 1):
         print(chr(ord('B') + i - 1), end=' ')
-        print_line(map_client, i)
+        print_line(map_atk, i, visible)
         print(' ' * 12, chr(ord('B') + i - 1), end=' ')
-        print_line(map_client, i)
+        print_line(map_def, i, visible)
         print()
 
 
-def print_map(matrix):
+def print_map(matrix, visible=False):
     print('Mapa \n', end='   ')
     for k in range(1, TAM - 1):
         print(str(k), end='   ' if k < 9 else '  ')
@@ -49,7 +52,7 @@ def print_map(matrix):
     print()
     for i in range(1, TAM - 1):
         print(chr(ord('B') + i - 1), end=' ')
-        print_line(matrix, i)
+        print_line(matrix, i, visible)
         print()
 
 
@@ -57,7 +60,7 @@ def init_maps() -> Tuple[list, list]:
     global map_def, map_atk
 
     map_atk.append([-1 for _ in range(TAM)])
-    for i in range(1, 13):
+    for __ in range(1, 13):
         line = [-1]
         line += [0 for _ in range(TAM - 2)]
         line.append(-1)
@@ -83,7 +86,8 @@ def get_position() -> list:
     A = 65
     coo = ['A', 0]
     while True:
-        poss = input('Digite a posição inicial da embarcação, EX: B1: ').upper()
+        poss = input(
+            'Digite a posição inicial da embarcação, EX: B1: ').upper()
         if len(poss) == 2 and poss[0].isalpha() and poss[1].isdigit():
             coo[0], coo[1] = ord(poss[0]) - A, int(poss[1])
 
@@ -98,14 +102,38 @@ def get_position() -> list:
 
 
 def init():
-    print('Batalha naval')
-    # connecting to server ----------
-    print('Peenchar sua mapa de defesa')
-    print_map(map_def)
+    global server, map_def
+    login = server.login()
+    if login > -1:
+        enbacations = {
+            'subimarinos': [2, 4],
+            'Cruzadores': [3, 3],
+            'Encouraçados': [4, 2],
+            'porta-aviões': [5, 1]}
 
-    orientation = get_orientation()
-    poss = get_position()
-    # enviar poss para o servidor, ele retornara se foi invalida ou n
+        print('Batalha naval')
+        print('Peenchar sua mapa de defesa')
+        print_map(map_def)
+
+        for e in enbacations.keys():
+            print('Peenchar os ' + e + ' ')
+            j = 0
+            while enbacations[e][1] != j:
+                orientation, poss = get_orientation(), get_position()
+
+                if server.set_poss(login, enbacations[e][0], poss, orientation):
+                    for i in range(enbacations[e][0]):
+                        if orientation == 'V':
+                            map_def[poss[0] + i][poss[1]] = enbacations[e][0]
+                        else:
+                            map_def[poss[0]][poss[1] + i] = enbacations[e][0]
+                    print_map(map_def, visible=True)
+                    j += 1
+                else:
+                    print('Posição invalida.')
+
+    else:
+        print('Sem vagas')
 
 
 '''
--- 3-periodo/P3/Python/batalha_naval/server/__init__.py
+++ 3-periodo/P3/Python/batalha_naval/server/__init__.py
@@ -1,14 +1,16 @@
 from xmlrpc.server import SimpleXMLRPCServer
 from copy import deepcopy
+from typing import Tuple
 
 
-map_player1 = []
-map_player2 = []
+maps = []
+players = -1
 TAM = 14
 
-
 # 12 X 12 com bordas
-def init_maps():
+
+
+def init_maps() -> list:
     player1 = [[-1 for _ in range(TAM)]]
 
     for _ in range(1, 13):
@@ -19,24 +21,65 @@ def init_maps():
 
     player1.append([-1 for _ in range(TAM)])
     player2 = deepcopy(player1)
-    return player1, player2
+    return [player1, player2]
 
 
-def is_valid():
-    pass
+def print_map(matrix):
+    for k in range(1, TAM - 1):
+        print(matrix[k])
 
 
-def fill_map():
-    pass
+def set_poss(player, siz, poss, orientation) -> bool:
+    print('Player: ', player)
+    print('Tamanho da embarcação: ', siz)
+    print('Posição: ', poss)
+    print('Orientação ', orientation)
+
+    length = (poss[0] -1, poss[0] + siz)
+    width = (poss[1] -1, poss[1] + 3)
+
+    if orientation == 'V':
+        length, width = width, length
+
+    print_map(maps[player])
+    print('Blocos: ')
+    for i in range(width[0], width[1]):
+        for j in range(length[0], length[1]):
+            print(maps[player][i][j])
+            if maps[player][i][j] != 0 and maps[player][i][j] != -1:
+                return False
+
+    for i in range(siz):
+        print('I: ', i)
+        if orientation == 'V':
+            maps[player][poss[0] + i][poss[1]] = siz
+        else:
+            maps[player][poss[0]][poss[1] + i] = siz
+    print_map(maps[player])
+
+    return True
+
+
+def login() -> int:
+    global players
+    if players < 2:
+        players += 1
+        return players
+
+    return 0
 
 
 def main():
     print('Server init...')
     server = SimpleXMLRPCServer(('0.0.0.0', 9999))
+
+    server.register_function(login)
+    server.register_function(set_poss)
+
     print('Ctrl + C to end')
     server.serve_forever()
 
 
 if __name__ == '__main__':
-    init_maps()
+    maps = init_maps()
     main()
